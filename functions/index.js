b00129f647391e454d79e60d6ba622aa12c734de
joaosf <t.joao.ferreira@zaffari.com.br>
2023-06-14 10:49:03 -0300

    updating firebaserc

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,8 @@
 
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const express = require('express');
+const cors = require('cors');
 const app = express();
 
 admin.initializeApp();
